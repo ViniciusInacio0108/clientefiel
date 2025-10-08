9b64b9985f7526fccbd6a59985f9090449a53bd3
Vinícius Inácio <vinicius.silva@grupobrisanet.com.br>
2025-10-08 20:57:40 -0300

    feat: added logged user and edit menu

--- src/main.py
+++ src/main.py
@@ -1,22 +1,34 @@
 from controllers.user_controller import UserController
 
 def menu():
-    controller = UserController()
+    user_controller = UserController()
+    logged_user = None
 
     while True:
         print("\n=== Sistema de Estabelecimentos ===")
-        print("1 - Cadastrar")
+        print("1 - Cadastrar estabelecimento")
         print("2 - Login")
+        if logged_user:
+            print("3 - Editar estabelecimento")
         print("0 - Sair")
 
         opcao = input("Escolha uma opção: ").strip()
 
         if opcao == '1':
-            controller.cadastrar()
+            user_controller.cadastrar()
         elif opcao == '2':
-            controller.login()
+            logged_user = user_controller.service.authenticate(
+                input("E-mail: ").strip(),
+                input("Senha: ").strip()
+            )
+            if logged_user:
+                print(f"Bem-vindo, {logged_user.name}!")
+            else:
+                print("E-mail ou senha incorretos.")
+        elif opcao == '3' and logged_user:
+            user_controller.edit_user_name(logged_user.email)
         elif opcao == '0':
-            print("Saindo...")
+            print("Encerrando...")
             break
         else:
             print("Opção inválida.")
